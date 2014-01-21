ce57c332b7dd5def4cdfb34ad8917a8efcb23440
Ben Willman <benjamin.willman@va.gov>
2014-01-21 13:52:05 -0500

    Validations Work...

---
+++ Model/WebForm21aServiceDateAnnotations.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Gov.Dva.Ogc.Data.Accreditation.Web.Model
+{
+    public class WebForm21aServiceDateAnnotations
+    {
+
+    }
+}
